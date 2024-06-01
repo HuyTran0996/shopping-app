0bf8c837786ad50f492a798def3ebbce360ce93c
HuyTran0996 <tranviquanghuy@gmail.com>
2024-06-01 21:24:10 +0700

    2024-06-01 footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,111 @@
 import React from "react";
 import "./Footer.scss";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+
+import { ImFacebook2 } from "react-icons/im";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-item">
+          <h6 className="title">Policies</h6>
+          <ul>
+            <li>
+              <a href="/terms" className="fs-15">
+                Terms & Conditions
+              </a>
+            </li>
+            <li>
+              <a href="/cookies" className="fs-15">
+                Cookies Policy
+              </a>
+            </li>
+            <li>
+              <a href="/policy" className="fs-15">
+                Data Policy
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-item">
+          <h6 className="title">About</h6>
+          <ul>
+            <li>
+              <a href="/terms" className="fs-15">
+                Company Info
+              </a>
+            </li>
+            <li>
+              <a href="/cookies" className="fs-15">
+                Branches
+              </a>
+            </li>
+            <li>
+              <a href="/policy" className="fs-15">
+                Store
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-item">
+          <h6 className="title">Contact</h6>
+          <ul>
+            <li>
+              <span>
+                <FaPhoneAlt size={20} />
+              </span>
+              <span className="fs-15">+123 546 789</span>
+            </li>
+            <li>
+              <span>
+                <IoIosMail size={25} />
+              </span>
+              <a href="mailto:info@shoplive.com">info@shoplive.com</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-item">
+          <h6 className="title">Follows Us</h6>
+          <ul>
+            <li>
+              <a
+                href="https://www.facebook.com/themoviedb/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <ImFacebook2 size={25} className="fab" /> Facebook
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/themoviedb/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaSquareXTwitter size={25} className="fab" /> X (Twitter)
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/themoviedb/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagramSquare size={25} className="fab" /> Instagram
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
